217ef249c873e4caa4cc140393ab6e014de6c318
nqtinh <nqtinh97@gmail.com>
2018-09-23 23:53:47 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -3,42 +3,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const size = 3;
+const nSquareToWin = 5;
+
 function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
+  return (props.win) ? (
+    <button className="square square-highlight" onClick={props.onClick}>
       {props.value}
     </button>
-  );
+  ) : (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>  
+  )  ;
 }
 
 class SquareRow extends React.Component {
   render() {
     let squareRow = this.props.row.map((square, idx) => {
       let k = "s" + idx;
-      // let win = false;
-      // let winner = this.props.winner;
-      // let rowIdx = this.props.rowIdx;
-      // if (winner) {
-      //   if (winner.direction === "ToRight" &&
-      //     idx >= winner.x && idx <= winner.x + nSquareToWin - 1 && rowIdx === winner.y) {
-      //       win = true;
-      //   }
-      //   if (winner.direction === "ToDown" &&
-      //       rowIdx >= winner.y && rowIdx <= winner.y + nSquareToWin - 1 && idx === winner.x) {
-      //       win = true;
-      //   }
-      //   if (winner.direction === "ToRightDown" &&
-      //     idx >= winner.x && idx <= winner.x + nSquareToWin - 1 && idx - winner.x === rowIdx - winner.y) {
-      //       win = true;
-      //   }
-      //   if (winner.direction === "ToLeftDown" &&
-      //     idx <= winner.x && idx >= winner.x - nSquareToWin + 1 && winner.x - idx === rowIdx - winner.y) {
-      //       console.log(winner.x+' '+winner.y+' '+idx+' '+rowIdx+' '+nSquareToWin);
-      //       win = true;
-      //   }
-      // }
+      let win = false;
+      let winner = this.props.winner;
+      let rowIdx = this.props.rowIdx;
+      if (winner) {
+        if (winner.direction === "Right" &&
+          idx >= winner.x && idx <= winner.x + nSquareToWin - 1 && rowIdx === winner.y) {
+            win = true;
+        }
+        if (winner.direction === "Down" &&
+            rowIdx >= winner.y && rowIdx <= winner.y + nSquareToWin - 1 && idx === winner.x) {
+            win = true;
+        }
+        if (winner.direction === "RightDown" &&
+          idx >= winner.x && idx <= winner.x + nSquareToWin - 1 && idx - winner.x === rowIdx - winner.y) {
+            win = true;
+        }
+        if (winner.direction === "LeftDown" &&
+          idx <= winner.x && idx >= winner.x - nSquareToWin + 1 && winner.x - idx === rowIdx - winner.y) {
+            console.log(winner.x+' '+winner.y+' '+idx+' '+rowIdx+' '+nSquareToWin);
+            win = true;
+        }
+      }
       return (
-        <Square  value={square} onClick={() => this.props.onClick(this.props.rowIdx, idx)} key={k} />
+        <Square win={win} value={square} onClick={() => this.props.onClick(this.props.rowIdx, idx)} key={k} />
       )
     })
     return (
@@ -78,9 +85,9 @@ class Board extends React.Component {
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    let tmpArr = Array(8);
-    for (let i = 0; i < 8; i++) {
-      tmpArr[i] = Array(8).fill(null);
+    let tmpArr = Array(size);
+    for (let i = 0; i < size; i++) {
+      tmpArr[i] = Array(size).fill(null);
     }
     this.state = {
       history: [{
@@ -102,9 +109,9 @@ class Game extends React.Component {
       squares[idx] = current.squares[idx].slice();
       return true;
     })
-    // if (calculateWinner(squares) || squares[i]) {
-    //   return;
-    // }
+    if (calculateWinner(squares) || squares[i][j]) {
+      return;
+    }
     squares[i][j] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{
@@ -130,7 +137,7 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    //const winner = calculateWinner(current.squares);
+    const winner = calculateWinner(current.squares);
 
     let moves = history.map((step, move) => {
       const desc = move ?
@@ -151,11 +158,11 @@ class Game extends React.Component {
     }
 
     let status;
-    // if (winner) {
-    //   status = 'Winner: ' + winner.val;
-    // } else {
-    //   status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    // }
+    if (winner) {
+      status = 'Winner: ' + winner.val;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     let arrow = this.state.isDescending ? '↓' : '↑'
 
     return (
@@ -164,7 +171,7 @@ class Game extends React.Component {
             <Board
               squares={current.squares}
               onClick={(i, j) => this.handleClick(i, j)}
-              //winner={winner}
+              winner={winner}
           />
         </div>
         <div className="game-info">
@@ -183,4 +190,51 @@ class Game extends React.Component {
 
 ReactDOM.render(<Game />, document.getElementById("root"));
 
-
+function calculateWinner(squares) {
+  let win;
+  for (let i = 0; i < squares.length; i++) {
+    for (let j = 0; j < squares[i].length; j++) {
+      //Kiểm trang NSquareToWin ô liên tiếp từ ô xuất phát sang phải, xuống góc phải dưới, xuống góc trái dưới
+      //Nếu có NSquareToWin - 1 cặp liên tiếp giống nhau thì thắng
+      //Direction: ToRight, ToRightDown, ToDown, ToLeftDown
+      if (!squares[i][j]) continue;
+      if (j <= squares[i].length - nSquareToWin) {
+        win = true;
+        for (let k = 0; k < nSquareToWin - 1; k++) {
+          if (squares[i][j + k] !== squares[i][j + k + 1]) {
+            win = false
+          }
+        }
+        if (win) return {val: squares[i][j], x: j, y: i, direction: 'Right'};
+      }
+      if (i <= squares.length - nSquareToWin) {
+        win = true;
+        for (let k = 0; k < nSquareToWin - 1; k++) {
+          if (squares[i + k][j] !== squares[i + k + 1][j]) {
+            win = false
+          }
+        }
+        if (win) return {val: squares[i][j], x: j, y: i, direction: 'Down'};
+      }
+      if (j <= squares[i].length - nSquareToWin && i <= squares.length - nSquareToWin) {
+        win = true;
+        for (let k = 0; k < nSquareToWin - 1; k++) {
+          if (squares[i + k][j + k] !== squares[i + k + 1][j + k + 1]) {
+            win = false
+          }
+        }
+        if (win) return {val: squares[i][j], x: j, y: i, direction: 'RightDown'};
+      }
+      if (i <= squares.length - nSquareToWin && j >= nSquareToWin - 1) {
+        win = true;
+        for (let k = 0; k < nSquareToWin - 1; k++) {
+          if (squares[i + k][j - k] !== squares[i + k + 1][j - k - 1]) {
+            win = false
+          }
+        }
+        if (win) return {val: squares[i][j], x: j, y: i, direction: 'LeftDown'};
+      }
+    }
+  }
+  return null;
+}
